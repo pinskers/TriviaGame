2b5e856a0cc49728295823c4974894d0bcb41354
Michelle Pinsky <michellepinsky@unboxedtechnology.com>
2017-11-27 15:54:53 -0500

    Adding pseudocode to javascript file.

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+// Initialize variables
+
+// Upon document startup display timer, and question and corresponding answers in their
+// respective divs.
+
+// Start timer.
+
+// If user selects the correct answer, display screen that shows "Congratulations! You chose the 
+// correct answer!"
+
+// After three seconds have passed, display next question and answer set.
+
+// If user selects the wrong answer, display screen that shows "Incorrect, the answer was [answer]."
+
+// After three seconds have passed, display next question and answer set.
+
+// If timer reaches zero, display screen that shows "You ran out of time, the answer was [answer]."
+
+// After three seconds have passed, display next question and answer set.
+
+// Once user answers last question and last correct/incorrect screen has been shown, display screen that
+// shows correct and incorrect answers with option to restart the game.
+
+// If user clicks restart, game restarts from the beginning.
+
